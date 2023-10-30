ccbcf06094b27dcd9f0779307ed28501ad452573
Lahiruka Wijesinghe <36619863+lahirukawijes@users.noreply.github.com>
2023-10-30 22:51:28 +0530

    update that script to send an email Alert to list of mail reeipients

--- Shell/searchString.sh
+++ Shell/searchString.sh
@@ -1,4 +1,3 @@
-#create a shell script to iterate through a defined file and search for a defined string pattern, then echo number of occurances
 #!/bin/bash
 
 # Define the file to search in
@@ -10,5 +9,15 @@ search_pattern="your_pattern"
 # Use grep to search for the pattern in the file and count the occurrences
 count=$(grep -o -c "$search_pattern" "$file_to_search")
 
-# Echo the number of occurrences
-echo "The string '$search_pattern' occurs $count times in $file_to_search."
+# Email settings
+recipient_emails=("recipient1@example.com" "recipient2@example.com")
+sender_email="sender@example.com"
+email_subject="Pattern Occurrence Alert"
+email_body="The string '$search_pattern' occurs $count times in $file_to_search."
+
+# Send the email alert
+for recipient in "${recipient_emails[@]}"; do
+  echo -e "$email_body" | mail -s "$email_subject" -a "From: $sender_email" "$recipient"
+done
+
+echo "Email alert sent to ${recipient_emails[@]}"
